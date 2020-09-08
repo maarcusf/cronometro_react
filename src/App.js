3d97d6908799d48ebe01bb049226e1d370c25f7d
maarcusf <mengo123>
2020-09-08 18:34:27 -0300

    Configurando iniciar, pausar e limpar cronômetro

--- src/App.js
+++ src/App.js
@@ -2,14 +2,51 @@ import React, { Component } from "react";
 import "./style.css";
 
 class App extends Component {
+
+constructor(props){
+    super(props);
+     this.state={
+        numero: 0,
+        estadoBotao: 'Iniciar'
+    };
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+    this.timer = null;
+}
+
+    vai(){
+        let state = this.state;
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+            state.estadoBotao = 'Iniciar';                        
+        } else {
+            this.timer = setInterval(()=>{
+                let state = this.state;
+                state.numero += 0.1;
+                this.setState(state);
+    
+            },100);
+            state.estadoBotao = 'Pausar';
+        }
+        this.setState(state);        
+    }
+
+    limpar(){
+        let state = this.state;
+        state.numero = 0;
+        
+        this.setState(state);
+    }
+
     render() {
         return (
             <div className="container">
                 <img src={require("./assets/cronometro.png")} className="img" />
-                <a className="timer">0.0</a>
+                <a className="timer">{this.state.numero.toFixed(1)}</a>
                 <div className="areaBtn">
-                    <a className="botao">VAI</a>
-                    <a className="botao">LIMPAR</a>
+                    <a className="botao" onClick={this.vai}>{this.state.estadoBotao}</a>
+                    <a className="botao" onClick={this.limpar}>LIMPAR</a>
                 </div>
             </div>
         );
